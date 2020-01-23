bd79ba9d60c9ec3bc5ebf5ebe11e790f3f9fe86b
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-01-23 10:40:32 -0500

    Update aris_apt.sh

--- unix/src/aris_apt.sh
+++ unix/src/aris_apt.sh
@@ -4,6 +4,6 @@
 # Initialization and update apt
 apt update
 apt -y upgrade
-apt -y install zsh wget curl net-tools emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm
+apt -y install zsh wget curl net-tools emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm cmake clang gdb valgrind build-essential
 
 echo ARIS APT COMPLETE
