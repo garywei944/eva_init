a09ec97c17825ad371f1f3d0b81760e18dc7605b
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-02-05 01:22:08 -0500

    Update aris_apt.sh

--- unix/src/aris_apt.sh
+++ unix/src/aris_apt.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# root
 
 # Initialization and update apt
-apt update
-apt -y upgrade
-apt -y install zsh wget curl net-tools zip emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm cmake clang gdb valgrind build-essential libgtest-dev
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install zsh wget curl net-tools zip emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm cmake clang gdb valgrind build-essential libgtest-dev
+
+# Install GooleTest
+cd /usr/src/googletest
+sudo mkdir build
+cd build
+sudo cmake ..
+sudo make
+sudo make install
+sudo ln -s /usr/local/lib/libgtest.a /usr/lib/libgtest.a
+sudo ln -s /usr/local/lib/libgtest_main.a /usr/lib/libgtest_main.a
 
 echo ARIS APT COMPLETE
