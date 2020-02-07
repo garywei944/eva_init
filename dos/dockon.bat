1b52d73d33787d1e0a46428c1f62fa5b05a2a37b
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-02-07 05:31:51 -0500

    Update dockon.bat

--- dos/dockon.bat
+++ dos/dockon.bat
@@ -1,1 +1,3 @@
-docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it -v E::/mnt/files mcorner/os377:latest bash
+REM docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it -v E::/mnt/files mcorner/os377:latest bash
+
+docker run -p 2201:22 --name os377 --cap-add SYS_PTRACE --security-opt seccomp=unconfined garywei944/os377:latest
