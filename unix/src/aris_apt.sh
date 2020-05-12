9bcf14518112c7034dd31f2dbde04acc5253e9eb
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-12 15:15:00 -0400

    Install Docker and openssh-server

--- unix/src/aris_apt.sh
+++ unix/src/aris_apt.sh
@@ -3,7 +3,8 @@
 # Initialization and update apt
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install zsh wget curl rsync net-tools zip emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm cmake clang gdb valgrind ctags build-essential libgtest-dev
+sudo apt remove -y --purge openssh-server
+sudo apt -y install zsh wget curl rsync net-tools zip emacs vim git git-flow screen dos2unix build-essential default-jdk python python-pip python3 python3-pip nodejs npm cmake clang gdb valgrind ctags build-essential libgtest-dev openssh-server
 
 # Install GooleTest
 cd /usr/src/googletest
@@ -14,5 +15,15 @@ sudo make
 sudo make install
 sudo ln -s /usr/local/lib/libgtest.a /usr/lib/libgtest.a
 sudo ln -s /usr/local/lib/libgtest_main.a /usr/lib/libgtest_main.a
+
+# Install Docker
+sudo apt remove -y docker docker-engine docker.io containerd runc
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo gpasswd -a ${USER} docker
 
 echo ARIS APT COMPLETE
