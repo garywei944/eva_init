d824635aa325819a2303f980776c3f441cb294a6
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-21 18:56:35 -0400

    Add init.sh

--- unix/aris_bak.sh
+++ unix/aris_bak.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# root
 
-tar cvpzf backup.tar.gz --exclude=/proc --exclude=/sys --exclude=/lost+found --exclude=/mnt --exclude=/media --exclude=/cdrom --exclude=backup.tar.gz /
+sudo tar cvpzf backup.tar.gz --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/lost+found --exclude=/tmp --exclude=/mnt --exclude=/media --exclude=/cdrom --exclude=backup.tar.gz --exclude=/swapfile --exclude=/var/lib/docker --exclude=/home/aris/vmware /
 
 echo ARIS BACKUP SUCCESS
