3920246a4b824b70c6c332c9ca1aa7e25481c7f9
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-22 12:25:12 -0400

    Update aris_bak.sh

--- unix/aris_bak.sh
+++ unix/aris_bak.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo tar cvpzf backup.tar.gz --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/lost+found --exclude=/tmp --exclude=/mnt --exclude=/media --exclude=/cdrom --exclude=backup.tar.gz --exclude=/swapfile --exclude=/var/lib/docker --exclude=/home/aris/vmware /
+sudo tar cvpzf /mnt/adam/backup.tar.gz --exclude=/proc --exclude=/sys --exclude=/dev --exclude=/lost+found --exclude=/tmp --exclude=/mnt --exclude=/media --exclude=/cdrom --exclude=backup.tar.gz /
 
 echo ARIS BACKUP SUCCESS
