26a1daf4440d879d7d1fa9375093f45c78bd515e
garywei944 <33930674+garywei944@users.noreply.github.com>
2020-05-29 16:09:04 -0400

    Install sublime-text via apt rather than snap

--- src/evapt.sh
+++ src/evapt.sh
@@ -119,14 +119,14 @@ chrome() {
 
 # Sublime Text 3 & Sublime Merge
 sublime() {
-	sudo snap install sublime-text --classic
+	# sudo snap install sublime-text --classic
 	# sudo snap install sublime-merge --classic
 
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo apt -y install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo apt update
-	# sudo apt -y install sublime-text
+	sudo apt -y install sublime-text
 	sudo apt -y install sublime-merge
 }
 
