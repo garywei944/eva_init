b90f3deb18af1fbf2c42614331868e972b4862b9
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-02-19 00:17:08 -0500

    Fix config_vim bug

--- src/config/vim.sh
+++ src/config/vim.sh
@@ -10,10 +10,15 @@ config_vim() {
   git clone https://github.com/scrooloose/nerdtree ~/.vim/pack/vendor/start/nerdtree
 
   # mru
+  git clone https://github.com/lvht/mru ~/.vim/pack/vendor/start/mru
+
+  # fzf
   sudo apt -y install fzf
+  git clone https://github.com/lvht/fzf ~/.vim/pack/vendor/start/fzf
 
   # ag
-  git clone https://github.com/lvht/fzf ~/.vim/pack/vendor/start/fzf
+  git clone https://github.com/mileszs/ack.vim ~/.vim/pack/vendor/start/ack
+  sudo apt -y install ack-grep silversearcher-ag
 
   cp -f "$CONFIG_DIR"/.vimrc ~
 }
