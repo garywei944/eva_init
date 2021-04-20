ed6e287dab6a1fd447f9efa27ed951ec4ac42251
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-20 19:48:21 -0400

    Install postgres

---
+++ src/apt/postgres.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+postgres() {
+	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+	sudo apt update
+	sudo apt install -y postgresql
+}
