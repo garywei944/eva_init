58f0d0e1e85fc8b5c434b03703f7b1ad81732d88
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-04-30 09:50:34 -0400

    Fix albert invalid gpg error

--- src/apt/albert.sh
+++ src/apt/albert.sh
@@ -2,19 +2,9 @@
 
 # Albert
 albert() {
-  # Ubuntu 18.04
-  # yes | sudo add-apt-repository -y ppa:noobslab/macbuntu
-  # sudo apt update
-  # sudo apt install -y albert
-
-  # # Make Albert autostart
-  # mkdir -p ~/.config/autostart/
-  # cp /usr/share/applications/albert.desktop ~/.config/autostart/
-  # chmod +x ~/.config/autostart/albert.desktop
-
   # Ubuntu 20.04
   echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-  curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home:manuelschneid3r.gpg >/dev/null
+  curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
   sudo apt update
   sudo apt install -y albert
 }
