2eb1779e7f01bb35589a0745966b0cebc98fa9d8
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-05-20 20:41:21 -0400

    Fix install wechat

--- src/apt/wechat.sh
+++ src/apt/wechat.sh
@@ -2,6 +2,6 @@
 
 wechat() {
 	wget -O- https://deepin-wine.i-m.dev/setup.sh | sh
-	sudo apt-get install com.qq.weixin.deepin
-	sudo apt install libjpeg62:i386
+	sudo apt install -y com.qq.weixin.deepin
+	sudo apt install -y libjpeg62:i386
 }
