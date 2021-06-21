052ec9a2fd5ef48eb46f2c693c56ad495d8e4063
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-06-20 21:48:05 -0400

    Fix shadowsocks

--- src/apt/shadowsocks.sh
+++ src/apt/shadowsocks.sh
@@ -1,13 +1,13 @@
 shadowsocks() {
 	[[ ${PORT+x} ]] || PORT=31705
-	[[ ${PASSWORD+x} ]] || read -sp 'Password: ' PASSWORD
+	[[ ${PASSWORD+x} ]] || read -sp 'Password for shadowsocks: ' PASSWORD
 
 	docker run \
 		--name eva_ss \
 		-e SERVER_ADDR=0.0.0.0 \
-		-p$(PORT):8388 \
-		-p$(PORT):8388/udp \
-		-e PASSWORD=$(PASSWORD) \
+		-p$PORT:8388 \
+		-p$PORT:8388/udp \
+		-e PASSWORD=$PASSWORD \
 		-e METHOD=chacha20-ietf-poly1305 \
 		-e TIMEOUT=60 \
 		-d shadowsocks/shadowsocks-libev
