709fac73d61909ee65a947b3bbc3a9b3a6a9b69b
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-07-01 02:34:38 -0400

    Fix .bashrc screwed up

--- src/config/sys.sh
+++ src/config/sys.sh
@@ -2,34 +2,40 @@
 
 # Configure sudo without password
 config_sudo() {
-  echo "Defaults	editor=/usr/bin/vim
+	echo "Defaults	editor=/usr/bin/vim
 $USER	ALL=(ALL)	NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 }
 
 clone_config() {
-  [[ ${EVA+x} ]] || exit
+	[[ ${EVA+x} ]] || exit
 
-  cd ~ || exit
-  rm -fr .git
-  git init
-  git remote add origin git@github.com:garywei944/eva_arch.git
-  git config core.excludesFile .eva.gitignore
-  git fetch
-  git reset --hard origin/main
-  git branch -m master main
-  git branch --set-upstream-to=origin/main main
+	cd ~ || exit
+	rm -fr .git
+	git init
+	git remote add origin git@github.com:garywei944/eva_arch.git
+	git config core.excludesFile .eva.gitignore
+	git fetch
+	git reset --hard origin/main
+	git branch -m master main
+	git branch --set-upstream-to=origin/main main
 }
 
 # Configuration Terminal
 config_terminal() {
-  [[ ${EVA+x} ]] || exit
+	[[ ${EVA+x} ]] || exit
 
-  # Configure zsh
-  chsh -s /bin/zsh || echo '[[ $SHLVL == 1 ]] && zsh && exit || exit' >> ~/.bashrc
-  # sudo vim /etc/passwd
-  wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh_custom/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh_custom/plugins/zsh-syntax-highlighting
+	# Configure zsh
+	chsh -s /bin/zsh || echo '
+if [[ -n $SSH_TTY && $SHLVL == 1 ]]; then
+	zsh
+	exit
+fi
+' >> ~/.bashrc
 
-  echo '. ~/.config/rc.zsh' > ~/.zshrc
+	# sudo vim /etc/passwd
+	wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh_custom/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh_custom/plugins/zsh-syntax-highlighting
+
+	echo '. ~/.config/rc.zsh' > ~/.zshrc
 }
