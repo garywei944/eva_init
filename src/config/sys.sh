76404293873271c97a006bd600f6d1fbe3020c92
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-03-06 19:18:31 -0500

    Add default ssh config

--- src/config/sys.sh
+++ src/config/sys.sh
@@ -2,40 +2,47 @@
 
 # Configure sudo without password
 config_sudo() {
-	echo "Defaults	editor=/usr/bin/vim
+  echo "Defaults	editor=/usr/bin/vim
 $USER	ALL=(ALL)	NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 }
 
 clone_config() {
-	[[ ${EVA+x} ]] || exit
+  [[ ${EVA+x} ]] || exit
 
-	cd ~ || exit
-	rm -fr .git
-	git init
-	git remote add origin git@github.com:garywei944/eva_arch.git
-	git config core.excludesFile .eva.gitignore
-	git fetch --depth=1
-	git reset --hard origin/main
-	git branch -m master main
-	git branch --set-upstream-to=origin/main main
+  cd ~ || exit
+  rm -fr .git
+  git init
+  git remote add origin git@github.com:garywei944/eva_arch.git
+  git config core.excludesFile .eva.gitignore
+  git fetch --depth=1
+  git reset --hard origin/main
+  git branch -m master main
+  git branch --set-upstream-to=origin/main main
 }
 
 # Configuration Terminal
 config_terminal() {
-	[[ ${EVA+x} ]] || exit
+  [[ ${EVA+x} ]] || exit
 
-	# Configure zsh
-	chsh -s /bin/zsh || echo '
+  # Configure zsh
+  chsh -s /bin/zsh || echo '
 if [[ -n $SSH_TTY && $SHLVL == 1 && -n $(command -v zsh) ]]; then
 	zsh
 	exit
 fi
-' >> ~/.bashrc
+' >>~/.bashrc
 
-	# sudo vim /etc/passwd
-	wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
-	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh_custom/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh_custom/plugins/zsh-syntax-highlighting
+  # sudo vim /etc/passwd
+  wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh_custom/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh_custom/plugins/zsh-syntax-highlighting
 
-	echo '. ~/.config/rc.zsh' > ~/.zshrc
+  echo '. ~/.config/rc.zsh' >~/.zshrc
+
+  # Config
+  if [[ ! -d ~/.ssh ]]; then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+  fi
+  echo 'Include ~/.config/ssh.conf' >>~/.ssh/config
 }
