8b5bfb1fc31a7d57b0514ffa756a85e98129389a
garywei944 <33930674+garywei944@users.noreply.github.com>
2023-03-27 15:27:04 -0400

    Update github ssh

--- src/config/git.sh
+++ src/config/git.sh
@@ -3,7 +3,9 @@
 # Add github.com to known_hosts
 config_git() {
   mkdir -p ~/.ssh
-  ssh-keyscan github.com >> ~/.ssh/known_hosts
+  ssh-keygen -R github.com
+  #  ssh-keyscan github.com >> ~/.ssh/known_hosts
+  curl -L https://api.github.com/meta | jq -r '.ssh_keys | .[]' | sed -e 's/^/github.com /' >>~/.ssh/known_hosts
   chmod 700 ~/.ssh
   chmod 644 ~/.ssh/known_hosts
 }
